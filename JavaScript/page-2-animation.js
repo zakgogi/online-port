bceb6e50561273a2ef1f5357a7e8bd138d87d462
Zak Gogi <zakgogi1@gmail.com>
2021-08-12 23:46:45 +0100

    added some animations to projects page

---
+++ JavaScript/page-2-animation.js
@@ -0,0 +1,9 @@
+const tl = gsap.timeline({delay: 0.3});
+tl.from("nav", {opacity: 0, duration: 1, y: -100} )
+tl.from("#bookingCard", {opacity: 0, duration: 0.3});
+tl.from("#journalismCard", {opacity: 0, duration: 0.3});
+tl.from("#sudokuCard", {opacity: 0, duration: 0.3});
+tl.from("#snakeCard", {opacity: 0, duration: 0.3});
+tl.from("#habitCard", {opacity: 0, duration: 0.3});
+tl.from("#sortCard", {opacity: 0, duration: 0.3});
+tl.from("#toggleButton", {opacity: 0, duration: 0.8, y: -100})
